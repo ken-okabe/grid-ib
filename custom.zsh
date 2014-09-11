ea73d19b158e30b10b3b01919d414b2251f34aa2
kenokabe <kenokabe@gmail.com>
2014-09-11 00:27:03 +0000

    commit

---
+++ custom.zsh
@@ -0,0 +1,21 @@
+uname -r
+
+ZSH_THEME="ys"
+
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/bin/java
+
+
+alias v="/usr/local/Cellar/vim/7.4.335/bin/vim"
+alias vi="v"
+alias vim="v"
+alias s='git add -A -v; git commit -m "commit"'
+alias sp="s; git push origin master"
+
+alias f="ranger"
+alias z="node app"
+alias x="node test"
+
+alias zz="browserify ./js/index.js -o ./js/index.bundle.js"
+alias a="atom ./"
+
+alias kn="killall -9 node"
